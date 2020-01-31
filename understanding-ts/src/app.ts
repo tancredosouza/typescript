3907760504eedf77b26a536d451ad20ccbcd55c7
Tancredo Souza <aqueletantan@gmail.com>
2020-01-30 20:18:48 -0800

    feature(understanding-ts): create function addWithDefaultValue

--- understanding-ts/src/app.ts
+++ understanding-ts/src/app.ts
@@ -15,3 +15,9 @@ console.log(applyThreeTimes(3, 5, add));
 const addCurrying = (n: number) => (a: number) => add(a, n);
 
 console.log(addCurrying(3)(1));
+
+function addWithDefaultValue(a: number, b:number = 3) {
+  return a + b;
+}
+
+console.log(addWithDefaultValue(5));
