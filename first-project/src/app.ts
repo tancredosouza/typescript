bcd97a923a2f2e6f08992cc9abe5c96e57354d0b
Tancredo Souza <aqueletantan@gmail.com>
2020-02-24 15:45:04 -0800

    Print user input when button gets pressed.

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -1,17 +1,10 @@
 // Code goes here!
 const PROJECT_INPUT_ID: string = "project-input";
-const SINGLE_PROJECT_ID = "single-project";
-const PROJECT_LIST_ID = "project-list";
 
 let templateElement = document.getElementById(
   PROJECT_INPUT_ID
 ) as HTMLTemplateElement;
 let clone = templateElement.content.cloneNode(true);
-
-//document.body.appendChild(clone);
-
-// displayTemplateOnWebPage(SINGLE_PROJECT_ID);
-// displayTemplateOnWebPage(PROJECT_LIST_ID);
 
 class Project {
   constructor(
@@ -29,9 +22,19 @@ let submitProjectButton = projectSubmissionForm.getElementsByTagName(
   "button"
 )[0] as HTMLButtonElement;
 
-submitProjectButton.addEventListener("click", function() {
-  console.log("Clicked!");
-});
+function createProjectFromUserInput() {
+  let divs = projectSubmissionForm.querySelectorAll(".form-control");
+
+  let title: string = divs[0].getElementsByTagName("input")[0].value;
+  let description: string = divs[1].getElementsByTagName("textarea")[0].value;
+  let teamSize: number = +divs[2].getElementsByTagName("input")[0].value;
+
+  console.log(title);
+  console.log(description);
+  console.log(teamSize);
+}
+
+submitProjectButton.addEventListener("click", createProjectFromUserInput);
 
 // render web page
 document.body.append(projectSubmissionForm);
